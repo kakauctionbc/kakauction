9ae6d8fefe08fdb97abf8a92280c652f32305311
gjnick <gjnick@gjnick-PC>
2016-09-19 01:13:16 +0900

    0919 푸시푸시

--- kakauction/src/main/java/com/app/kaka/admin/freeboard/model/AdminFreeboardDAOMybatis.java
+++ kakauction/src/main/java/com/app/kaka/admin/freeboard/model/AdminFreeboardDAOMybatis.java
@@ -1,9 +1,11 @@
 package com.app.kaka.admin.freeboard.model;
 
 import org.mybatis.spring.support.SqlSessionDaoSupport;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public class AdminFreeboardDAOMybatis extends SqlSessionDaoSupport implements AdminFreeboardDAO {
-	
+	private Logger logger = LoggerFactory.getLogger(AdminFreeboardDAOMybatis.class);
 }
